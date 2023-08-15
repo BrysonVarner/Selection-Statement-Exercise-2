93096a26d1b7908b52897802b55fb248accfd09f
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-15 12:51:38 -0500

    Completed Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is always an interesting subject!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is a difficult subject, so much to remember");
+                    break;
+                case "p.e.":                
+                    Console.WriteLine("P.E. is always good to get you up and moving!");
+                    break;
+                case "pe":
+                    Console.WriteLine("P.E. is always good to get you up and moving!");
+                    break;
+                default:
+                    Console.WriteLine($"Wow! I havent taken that subject before! {subject} sounds fun!");
+                    break;
+           
+            }
         }
     }
 }
